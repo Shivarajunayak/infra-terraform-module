c79aecae51c40660452817e001caa4084d0d4a00
shivaraju <shivaraju5300@gmail.com>
2024-04-29 10:45:53 +0530

    lambda-stack

---
+++ module/lambda-stack/main.tf
@@ -0,0 +1,13 @@
+module "lambda_function_existing_package_s3" {
+  source = "./terraform-aws-lambda"
+
+  function_name = var.function_name
+  description   = var.description
+  handler       = var.handler
+  runtime       = var.runtime
+
+  create_package      = var.create_package
+  s3_existing_package = var.s3_existing_package
+  tags                = var.tags
+}
+
