c3a41942dd2cc15e35e9273e040931a4c6f154ba
Shiavaraju Nayak <51288760+Shivarajunayak@users.noreply.github.com>
2024-04-30 15:54:20 +0530

    Update main.tf

--- module/lambda-stack/main.tf
+++ module/lambda-stack/main.tf
@@ -1,13 +1,18 @@
 module "lambda_function_existing_package_s3" {
-  source = "./terraform-aws-lambda"
-
-  function_name = var.function_name
-  description   = var.description
-  handler       = var.handler
-  runtime       = var.runtime
-
-  create_package      = var.create_package
-  s3_existing_package = var.s3_existing_package
-  tags                = var.tags
+  source = "./terraform-aws-lambda-master"
+  function_name = "my-lambda1"
+  description   = "My awesome lambda function"
+  handler       = "index.lambda_handler"
+  runtime       = "python3.12"
+  
+create_package      = false
+  s3_existing_package = {
+    bucket = "testlambda0921"
+    key    = "my-function.zip"
+  }
+ 
+tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
-
